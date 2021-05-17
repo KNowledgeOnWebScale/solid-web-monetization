b4259d653e456bd1e165381866415b008486a6a2
Thomas Dupont <thomas.dupont@intec.ugent.be>
2021-05-17 18:08:09 +0200

    remove comment

--- solid-accountant/src/App.tsx
+++ solid-accountant/src/App.tsx
@@ -8,15 +8,6 @@ import "./App.scss";
 import { About, Home, Wallet } from "./pages";
 import MyNav from "./MyNav";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
