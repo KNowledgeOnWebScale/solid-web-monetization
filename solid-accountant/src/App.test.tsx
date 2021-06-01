43985dc264cb8dd391ad715a7007e5112edfa5c2
Thomas Dupont <thomas.dupont@intec.ugent.be>
2021-06-01 13:37:05 +0200

    tests fixed

--- solid-accountant/src/App.test.tsx
+++ solid-accountant/src/App.test.tsx
@@ -4,10 +4,12 @@ import App from './App';
 
 test('renders navigation', () => {
     const { container } = render(<App />);
-    const home = container.querySelector('a[href*="/"]');
-    const wallet = container.querySelector('a[href*="/wallet" i]');
-    const about = container.querySelector('a[href*="/about" i]');
+    const home = container.querySelector('a[href*="#"]');
+    const wallet = container.querySelector('a[href*="#wallet" i]');
+    const about = container.querySelector('a[href*="#about" i]');
+    const auth = container.querySelector('a[href*="#auth" i]');
     expect(home).toBeInTheDocument();
     expect(wallet).toBeInTheDocument();
     expect(about).toBeInTheDocument();
+    expect(auth).toBeInTheDocument();
 });
