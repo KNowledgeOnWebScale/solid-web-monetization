156861a3e3ebabd68a356fd44f2be2ca54cd58f4
Thomas Dupont <thomas.dupont@intec.ugent.be>
2021-11-18 11:53:18 +0100

    websocket test working

--- solid-microstore/src/app/paywall/paywall.component.ts
+++ solid-microstore/src/app/paywall/paywall.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MonetizationService } from '../monetization.service';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-paywall',
@@ -7,6 +8,7 @@ import { MonetizationService } from '../monetization.service';
   styleUrls: ['./paywall.component.scss']
 })
 export class PaywallComponent implements OnInit {
+  private sock: WebSocket | null = null;
 
   constructor(private money: MonetizationService) { }
 
@@ -14,6 +16,22 @@ export class PaywallComponent implements OnInit {
     // this.money.setPaymentPointer('$rafiki.money/p/thomas.dupont@ugent.be');
     // this.money.setPaymentPointer('$rafiki.money/p/tdupont@ugent.be');
     this.money.events.subscribe(console.log)
+    this.sock = new SockJS('http://localhost:8888/ws');
+
+    this.sock.onopen = function () {
+      console.log('open');
+      this.send('test');
+    };
+
+    this.sock.onmessage = function (e) {
+      console.log('message', e.data);
+      this.close();
+    };
+
+    this.sock.onclose = function () {
+      console.log('close');
+    };
+
   }
 
   isMonetizationAvailable(): boolean {
