215a85f1f4bf92bfda8944c10f2f0431dc0075d3
Thomas Dupont <thomas.dupont@ugent.be>
2022-01-24 11:09:25 +0100

    Image generator added

--- solid-microstore/src/app/mix/mix.component.ts
+++ solid-microstore/src/app/mix/mix.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneratedImage, ImageService } from '../image.service';
 import { WmPService } from '../wmp.service';
 
 @Component({
   selector: 'app-mix',
   templateUrl: './mix.component.html',
-  styleUrls: ['./mix.component.scss']
+  styleUrls: ['./mix.component.scss'],
 })
 export class MixComponent implements OnInit {
+  images: GeneratedImage[] = [];
 
-  constructor(private wm: WmPService) { }
+  constructor(private wm: WmPService, private img: ImageService) { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 20; i++) {
+      this.images.push(this.img.getRandomImage());  
+    }
   }
 
   isMonetizationAvailable(): boolean {
